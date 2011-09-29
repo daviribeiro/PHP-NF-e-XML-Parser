7d451cd208645498f27170aadbd6c02ab060b82b
daviribeiro <daviantunes27@hotmail.com>
2011-09-28 23:23:24 -0300

    GPLv2.txt 	css/ 	database.sql 	entrada/ 	includes/ 	painel/ 	saida/

---
+++ index.php
@@ -0,0 +1,56 @@
+<!--
+
+
+Este software foi escrito por davi Antunes Ribeiro ( daviantunes27@hotmail.com )e é baseado no pacote nfePHP http://www.nfephp.org/ e no Twitter Bootstrap ( http://twitter.github.com/bootstrap/ )
+sendo livre para alteração e distribuiução conforme os termos da GPL v2 disponivel junto com este.
+
+
+--> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>Sistema de Importação de XML e Calculos de Impostos de Notas Ficais</title>
+      <meta charset="utf-8"/>
+    
+    <meta name="description" content="Sistema para Importação de XMLs para Calculos de impostos diversos - Entrada e Saida"/>
+    <meta name="author" content="Davi Antunes Ribeiro"/>
+
+    
+    <link href="css/bootstrap-1.2.0.css" rel="stylesheet"/>
+  </head>
+
+  <body>
+
+<div class="topbar">
+      <div class="fill">
+        <div class="container">
+
+          <ul class="nav">
+              <!-- Opção de inclusao de logo da empresa: 
+              <li><img src="images/logo-cubo.jpg" alt=""/></li>-->
+             <li class="active"><a href="#">Home</a></li>
+            <li><a href="entrada">Carga de XMLs de Entrada</a></li>
+            <li><a href="saida">Carga de XMLs de Saida</a></li>
+            <li><a href="painel">Painel Totalizador de Impostos</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div class="hero-unit">
+           <img src="images/logo.jpg" alt=""/>
+          <h2>Conversor de arquivos XML para calculo de impostos</h2>
+        <p>Este programa foi desenvolvido para exportar arquivos XMLs para soma e cálculo de impostos, tais como PIS, COFINS, etc.</p>
+        <p>Para utilizar o mesmo, clique em uma das funções acima (na barra superior do site).</p>
+      </div>
+
+
+      <footer>
+        <p>&copy; </p>
+      </footer>
+
+    </div> <!-- /container -->
+
+  </body>
+</html>
